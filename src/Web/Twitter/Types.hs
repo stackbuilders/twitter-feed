9e318cb2aa40ae0caabfba4ad6a647ac010bfb32
Sebastián Estrella <sebas007estrella@gmail.com>
2014-11-20 11:37:42 -0500

    [JPV,SE] Add Haddock documentation

--- src/Web/Twitter/Types.hs
+++ src/Web/Twitter/Types.hs
@@ -1,6 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
 
+---------------------------------------------------------
+-- |
+-- Module     : Web.Twitter.Types
+-- License    : MIT (see the file LICENSE)
+-- Maintainer : Justin Leitgeb <justin@stackbuilders.com>
+--
+-- Types for working with the twitter API.
+---------------------------------------------------------
 module Web.Twitter.Types
   ( Tweet(..)
   , SimpleTweet(..)
@@ -17,48 +25,56 @@ import Data.Aeson.Types
 import Data.Ord (comparing)
 import GHC.Generics (Generic)
 
+-- | Represents tweet start and end index
 type BoundingIndices = (Int, Int)
 
+-- | Represents a tweet as HTML
 data SimpleTweet = SimpleTweet
-  { body       :: String
-  , tweetId    :: String
-  , created_at :: String
+  { body       :: String -- ^ Tweet content as HTML
+  , tweetId    :: String -- ^ Tweet id
+  , created_at :: String -- ^ Tweet created at date
   } deriving (Show, Generic)
 
+-- | Represent a tweet as plain text
 data Tweet =  Tweet
-  { text       :: String
-  , createdAt  :: String
-  , idStr      :: String
-  , entities   :: Entities
+  { text       :: String    -- ^ Tweet content as plain text
+  , createdAt  :: String    -- ^ Tweet created at date
+  , idStr      :: String    -- ^ Tweet id
+  , entities   :: Entities  -- ^ All references mentioned on the tweet
   } deriving (Show, Generic)
 
+-- | Represent a list of all tweet references
 data Entities = Entities
-  { userEntities  :: [UserEntity]
-  , urlEntities   :: [URLEntity]
-  , mediaEntities :: [MediaEntity]
+  { userEntities  :: [UserEntity]  -- ^ List of all user references mentioned on the tweet
+  , urlEntities   :: [URLEntity]   -- ^ List of all urls references mentioned on the tweet
+  , mediaEntities :: [MediaEntity] -- ^ List of all media references mentioned on the tweet
   } deriving (Show, Generic)
 
+-- | Represent a user reference
 data UserEntity = UserEntity
-  { screenName  :: String
-  , userIndices :: BoundingIndices
+  { screenName  :: String          -- ^ User twitter name
+  , userIndices :: BoundingIndices -- ^ Tweet start and end index
   } deriving (Show, Generic)
 
+-- | Represent a url reference
 data URLEntity = URLEntity
-  { urlMessage   :: String
-  , urlIndices   :: BoundingIndices
-  , displayUrl   :: String
+  { urlMessage   :: String          -- ^ Reference title
+  , urlIndices   :: BoundingIndices -- ^ Tweet start and end index
+  , displayUrl   :: String          -- ^ Reference url
   } deriving (Show, Generic)
 
+-- | Represent a media reference
 data MediaEntity = MediaEntity
-  { mediaUrl        :: String
-  , mediaIndices    :: BoundingIndices
-  , displayMediaUrl :: String
+  { mediaUrl        :: String          -- ^ Reference url
+  , mediaIndices    :: BoundingIndices -- ^ Tweet start and end index
+  , displayMediaUrl :: String          -- ^ Reference title
   } deriving (Show, Generic)
 
+-- | Represent a link as HTML
 data Link = Link
-  { startIndex :: Int
-  , endIndex   :: Int
-  , newHtml    :: String
+  { startIndex :: Int    -- ^ Tweet start index
+  , endIndex   :: Int    -- ^ Tweet end index
+  , newHtml    :: String -- ^ Link as HTML
   } deriving (Show, Eq)
 
 instance ToJSON SimpleTweet
